e29421fbb68c22ad10f945c10e638dbe309775e5
IxxyDev <denisenkoforwork@gmail.com>
2025-03-17 22:38:09 +0300

    feat: connect dispatcher with rendering

--- packages/runtime/src/app.js
+++ packages/runtime/src/app.js
@@ -1,16 +1,34 @@
 import { destroyDOM } from "./destroyDOM"
+import { Dispatcher } from "./dispatcher"
 import { mountDOM } from "./mountDOM"
 
-export function createApp({ state, view }) {
+export function createApp({ state, view, reducers = {} }) {
 	let parentElem = null
 	let vdom = null
+
+	const dispatcher = new Dispatcher()
+	const subscriptions = [dispatcher.afterEveryCommand(renderApp)]
+
+	function emit(evName, payload) {
+		dispatcher.dispatch(evName, payload)
+	}
+
+	for (const actionName in reducers) {
+		const reducer = reducers[actionName]
+
+		const subs = dispatcher.subscribe(actionName, (payload) => {
+			state = reducer(state, payload)
+		})
+
+		subscriptions.push(subs)
+	}
 
 	function renderApp() {
 		if (vdom) {
 			destroyDOM(vdom)
 		}
 
-		vdom = view(state)
+		vdom = view(state, emit)
 		mountDOM(vdom, parentElem)
 	}
 
